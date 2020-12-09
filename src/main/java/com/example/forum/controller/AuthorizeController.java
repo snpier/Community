070a8a506b1b0337f661f3a5da9690a947d2eeab
sniper <952387936@qq.com>
2020-12-09 15:55:09 +0800

    inster publish

--- src/main/java/com/example/forum/controller/AuthorizeController.java
+++ src/main/java/com/example/forum/controller/AuthorizeController.java
@@ -10,14 +10,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-<<<<<<< HEAD
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-=======
-import javax.servlet.http.HttpServletRequest;
->>>>>>> 59655105a719245507fcda21fee8427fdf653442
 import java.util.UUID;
 
 @Controller
@@ -41,12 +36,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-<<<<<<< HEAD
-                           HttpServletRequest request,
-                           HttpServletResponse response){
-=======
+                           HttpServletResponse response,
                            HttpServletRequest request){
->>>>>>> 59655105a719245507fcda21fee8427fdf653442
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -59,7 +50,6 @@ public class AuthorizeController {
             User user = new User();
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
-<<<<<<< HEAD
             String token = UUID.randomUUID().toString();
             user.setToken(token);
             user.setGmtCreate(System.currentTimeMillis());
@@ -68,14 +58,12 @@ public class AuthorizeController {
             response.addCookie(new Cookie("token",token));
             //登录成功，写入cookies 和 session
 //            request.getSession().setAttribute("user",githubUser);
-=======
             user.setToken(UUID.randomUUID().toString());
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtMofifed(user.getGmtCreate());
             userMapper.inster(user);
             //登录成功，写入cookies 和 session
             request.getSession().setAttribute("user",githubUser);
->>>>>>> 59655105a719245507fcda21fee8427fdf653442
             return "redirect:/";
         } else {
             //登录失败，重新登陆
--- src/main/java/com/example/forum/mapper/UserMapper.java
+++ src/main/java/com/example/forum/mapper/UserMapper.java
@@ -3,20 +3,13 @@ package com.example.forum.mapper;
 import com.example.forum.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-<<<<<<< HEAD
 import org.apache.ibatis.annotations.Select;
-=======
->>>>>>> 59655105a719245507fcda21fee8427fdf653442
 
 @Mapper
 public interface UserMapper {
     @Insert("inster into user (name,accountId,token,gmtCreate,gmtMofifed) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtMofifed})")
     void inster(User user);
-<<<<<<< HEAD
 
     @Select("select * from user where token = #{token}")
     User findByToken(String token);
-
-=======
->>>>>>> 59655105a719245507fcda21fee8427fdf653442
 }
